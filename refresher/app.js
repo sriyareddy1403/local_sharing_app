c394a7a50dcef7676b0abd8703492694d519402a
reddysriya <sriya.mareddy@gmail.com>
2022-09-18 23:36:41 +0530

    first commit

---
+++ refresher/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+//const { createProduct, getProducts } = require("./mongodb");
+const { createProduct, getProducts } = require("./mongoose");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.post("/products", createProduct);
+app.get("/products", getProducts);
+
+app.listen(3000);
